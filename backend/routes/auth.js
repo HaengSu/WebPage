2b1e6a58405d9a2945314a1c07eff6dafde14089
HaengSu <onse3840@gmail.com>
2025-09-07 12:16:03 +0900

    add login api

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -18,7 +18,7 @@ router.post("/register", async (req, res) => {
 
     // DB에 사용자 저장
     const sql = "INSERT INTO users (email, password,nickname, purpose, level) VALUES (?, ?, ? ,? , ?)";
-    db.query(sql, [email, hashedPassword,nickname, purpose, level ], (err, result) => {
+    db.query(sql, [email, hashedPassword, nickname, purpose, level], (err, result) => {
       if (err) {
         if (err.code === "ER_DUP_ENTRY") {
           return res.status(400).json({ message: "이미 가입된 이메일입니다." });
@@ -33,5 +33,48 @@ router.post("/register", async (req, res) => {
     return res.status(500).json({ message: "서버 에러" });
   }
 });
+
+router.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  if (email == false || password == false) {
+    return res.status(400).json({ message: '이메일과 비밀번호를 입력하세요.' });
+  }
+
+  try {
+
+    const sql = "SELECT * FROM users WHERE email = ?";
+    db.query(sql, [email], async (err, result) => {
+      if (err) {
+        console.error("로그인 에러 : " + err);
+        return res.status(500).json({ message: '서버 에러' })
+      }
+
+      if (result.length === 0) {
+        return res.status(401).json({ message: '존재하지 않는 이메일 입니다.' });
+      }
+
+      const user = result[0];
+
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (isMatch == false) {
+        return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
+      }
+
+      return res.status(200).json({
+        message: '로그인 성공', user: {
+          email: user.email,
+          nickname: user.nickname,
+          purpose: user.purpose,
+          level : user.level
+        }
+      });
+    })
+  } catch(error) {
+    console.error('서버에러 : ',error);
+    return res.status(500).json({message : '서버 에러'});
+  }
+})
 
 module.exports = router;
